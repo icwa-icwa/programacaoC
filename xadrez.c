88b5b81a0655e03a61ee9455bca26a6642ba1e5e
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-05-30 19:32:28 +0000

    escrevendo desafio

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,16 @@
 
 int main(){
     printf("Desafio Xadrez!\n");
+    char Estado[1], Código_da_Carta[2];
+    char Nome_da_Cidade[20];
+    int População, Número_de_Pontos_Turísticos;
+    float Área_em_Km2, PIB;
+    printf("Digite a Inicial do Estado: \n");
+    scanf("%s",&Estado);
+
+    
+    
+
 
 
 }
