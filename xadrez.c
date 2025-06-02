82a72a8607090cce497689f4473a8da27ef20c6c
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-06-02 18:13:57 +0000

    CONCLUSAO

--- xadrez.c
+++ xadrez.c
@@ -2,10 +2,33 @@
 
 int main(){
     printf("Desafio Xadrez!\n");
-    char Estado[1], Codigo_da_Carta[3], Nome_da_Cidade[20];
+    char Estado[50], Codigo_da_Carta[50], Nome_da_Cidade[50];
     int Populacao, Numero_de_Pontos_Turisticos;
     float Area_em_Km2, PIB;
 
+    printf("Carta 1: \n");
+    printf("Digite a Inicial do Estado: \n");
+    scanf("%s", &Estado);
+   
+    printf("Digite o Codigo: \n");
+    scanf("%s", &Codigo_da_Carta);
+   
+    printf("Digite o Nome da Cidade: \n");
+    scanf("%s", &Nome_da_Cidade);
+   
+    printf("Digite a População: \n");
+    scanf("%d", &Populacao);
+    
+    printf("Digite a Área em Km2: \n");
+    scanf("%f", &Area_em_Km2);
+    
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+    
+    printf("Digite o Número de Pontos Turísticos: \n");
+    scanf("%d", &Numero_de_Pontos_Turisticos);
+
+    printf("Carta 2: \n");
     printf("Digite a Inicial do Estado: \n");
     scanf("%s", &Estado);
    
@@ -27,13 +50,25 @@ int main(){
     printf("Digite o Número de Pontos Turísticos: \n");
     scanf("%d", &Numero_de_Pontos_Turisticos);
     
-    printf("A inicial do Estado é: %s \n", Estado);
-    printf("O Codigo da Carta é: %s", Codigo_da_Carta);
-    printf("O Nome da Cidade é: %s \n", Nome_da_Cidade);
+    printf("Carta1: \n");
+    printf("Estado: %s \n", Estado);
+    printf("Código: %s \n", Codigo_da_Carta);
+    printf("Nome da Cidade: %s \n", Nome_da_Cidade);
     printf("População: %d \n", Populacao);
-    printf("Área: %f \n", Area_em_Km2);
-    printf("PIB: %f \n", PIB);
+    printf("Área: %.2f \n", Area_em_Km2);
+    printf("PIB: %.2f \n", PIB);
     printf("Número de Pontos Turísticos: %d \n", Numero_de_Pontos_Turisticos);
+    
+    printf("Carta2: \n");
+    printf("Estado: %s \n", Estado);
+    printf("Código: %s \n", Codigo_da_Carta);
+    printf("Nome da Cidade: %s \n", Nome_da_Cidade);
+    printf("População: %d \n", Populacao);
+    printf("Área: %.2f \n", Area_em_Km2);
+    printf("PIB: %.2f \n", PIB);
+    printf("Número de Pontos Turísticos: %d \n", Numero_de_Pontos_Turisticos);
+
+    return 0;
 
 
     
