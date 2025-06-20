5a42a5c138750991fac323c972dafa978ce3ec6c
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-06-20 10:32:23 +0000

    hora de codar mestre

---
+++ binary.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+//tema 3 hora de codar mestre
+int numeroJogador, numeroComputador, resultado;
+char tipoComparacao;
+
+// Gerar numero aleatorio
+srand(time(0));
+numeroComputador = rand() % 100 +1;//Numero entre 1 e 100
+// inicio de jogo
+printf("Bem-vindo ao jogo Maior, Menor ou Igual!\n");
+printf("Você deve escolher um número e o tipo de comparação.\n");
+printf("M. Maior\n");
+printf("N. Menor\n");
+printf("I. Igual\n");
+
+printf("Escolha a comparação: ");
+scanf(" %c", &numeroJogador);
+
+// exibir numero do computador
+printf("O número do computador é: %d\n", numeroComputador);
+scanf("%c", &numeroComputador);
+
+switch(tipoComparacao){
+    case 'M':
+    case 'm':
+        printf("Você escolheu a opção maior!\n");
+        resultado = numeroJogador > numeroComputador ? 1 : 0;
+        break;
+
+    case 'N':
+    case 'n':
+        printf("Você escolheu a opção menor!\n");
+        resultado = numeroJogador < numeroComputador ? 1 : 0;
+        break;
+
+    case 'I':
+    case 'i':
+        printf("Você escolheu a opção igual!\n");
+        resultado = numeroJogador == numeroComputador ? 1 : 0;
+        break;
+
+    
+    default:
+    printf("Opção de jogo inválida. \n");
+    break;
+
+if (resultado == 1)
+    {printf("Parabéns, você venceu! \n");
+    } else
+ {printf("Infelizmente, você perdeu!\n");}
+
+
+
+}
+
+
+return 0;
+
+}
