ca86b7955e3b9a96706fed89ac155558fbb5726a
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-07-11 11:22:18 +0000

    bispo

--- xadrez.c
+++ xadrez.c
@@ -1,81 +1,10 @@
 #include <stdio.h>
-
 int main(){
-    printf("Desafio Xadrez!\n");
-    char Estado[50], Codigo_da_Carta[50], Nome_da_Cidade[50];
-    int Populacao, Numero_de_Pontos_Turisticos;
-    float Area_em_Km2, PIB;
+for (int b = 0, j = 0; b<6, j<6; b++, j++) { // simulação do movimento do bispo na diagonal por 5 casas
+    printf("direita\n cima\n");
+    b++;
+    j++;
+}
 
-    printf("Carta 1: \n");
-    printf("Digite a Inicial do Estado: \n");
-    scanf("%s", &Estado);
-   
-    printf("Digite o Código: \n");
-    scanf("%s", &Codigo_da_Carta);
-   
-    printf("Digite o Nome da Cidade: \n");
-    scanf("%s", &Nome_da_Cidade);
-   
-    printf("Digite a População: \n");
-    scanf("%d", &Populacao);
-    
-    printf("Digite a Área em Km2: \n");
-    scanf("%f", &Area_em_Km2);
-    
-    printf("Digite o PIB: \n");
-    scanf("%f", &PIB);
-    
-    printf("Digite o Número de Pontos Turísticos: \n");
-    scanf("%d", &Numero_de_Pontos_Turisticos);
-
-    printf("Carta 2: \n");
-    printf("Digite a Inicial do Estado: \n");
-    scanf("%s", &Estado);
-   
-    printf("Digite o Código: \n");
-    scanf("%s", &Codigo_da_Carta);
-   
-    printf("Digite o Nome da Cidade: \n");
-    scanf("%s", &Nome_da_Cidade);
-   
-    printf("Digite a População: \n");
-    scanf("%d", &Populacao);
-    
-    printf("Digite a Área em Km2: \n");
-    scanf("%f", &Area_em_Km2);
-    
-    printf("Digite o PIB: \n");
-    scanf("%f", &PIB);
-    
-    printf("Digite o Número de Pontos Turísticos: \n");
-    scanf("%d", &Numero_de_Pontos_Turisticos);
-    
-    printf("Carta1: \n");
-    printf("Estado: %s \n", Estado);
-    printf("Código: %s \n", Codigo_da_Carta);
-    printf("Nome da Cidade: %s \n", Nome_da_Cidade);
-    printf("População: %d \n", Populacao);
-    printf("Área: %.2f \n", Area_em_Km2);
-    printf("PIB: %.2f \n", PIB);
-    printf("Número de Pontos Turísticos: %d \n", Numero_de_Pontos_Turisticos);
-    
-    printf("Carta2: \n");
-    printf("Estado: %s \n", Estado);
-    printf("Código: %s \n", Codigo_da_Carta);
-    printf("Nome da Cidade: %s \n", Nome_da_Cidade);
-    printf("População: %d \n", Populacao);
-    printf("Área: %.2f \n", Area_em_Km2);
-    printf("PIB: %.2f \n", PIB);
-    printf("Número de Pontos Turísticos: %d \n", Numero_de_Pontos_Turisticos);
-
-    return 0;
-
-
-    
-
-    
-    
-
-
-
+return 0;
 }
