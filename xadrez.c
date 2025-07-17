56eaa0944ddf9c7ee25cb3380d062cb6227fb2f6
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-07-17 21:35:52 +0000

    array

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 int main(){
-for (int b = 0, j = 0; b <= 5, j <= 5; b++, j++) { // simulação do movimento do bispo
-    printf("direita\n cima\n");//na diagonal 1 casa p/direita e 1 p/cima 5 vezes
+    // nível novato 
+    printf("***Simulação do Movimento do Bispo***\n");
+for (int b = 0; b <= 5; b++) { // simulação do movimento do bispo
+    printf("direita\n cima\n");//na diagonal 1 casa p/direita e 1 p/cima por 5 casas
 }
+// simulação do movimento da torre 5 casas para a direita
+printf("***Simulação do Movimento da Torre***\n");
+int t = 0;
+while (t < 5){
+    printf("direita\n");
+    t++;
+}
+// simulação do movimento da rainha 8 casas para a esquerda
+printf("***Simulação do Movimento da Rainha***\n");
+int r = 0;
+do {
+    printf("esquerda\n");
+    r++;
+} while(r<8);
 
 return 0;
 }
