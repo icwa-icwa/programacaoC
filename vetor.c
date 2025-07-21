39f919898997a32fbc4779e38fa88b7f376973f4
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-07-21 18:53:58 +0000

    novas implementações com constantes

--- vetor.c
+++ vetor.c
@@ -11,7 +11,7 @@ int main () {
         printf("  %c", coluna[j]);// deve ter um espaço depois das aspas
     }
     printf("\n");
-    printf("\n");*/
+    printf("\n");
     int vetor[5] = {1, 2, 3, 4, 5};
 
     for (int j = 0; j < 5; j++){
@@ -23,11 +23,48 @@ int main () {
     tive que mandar pular linha fora do for
     e iniciei outro for
     tive que reiniciar o vetor para comandar uma outra expressão e imprimir */
-    printf("\n");
+    /*printf("\n");
     for(int i = 0; i < 5; i++){
         
         printf(" %d", i*2, vetor[i]);// se chamar o i ele dá o valor do índice e nesse caso não precisa reiniciar o vetor
     }
     printf("\n");
+
+    int matriz[3][3] = {{9,15,21}, {2,4,6},{2,3,5}};
+    int somamatriz[3][3];
+    for(int i = 0; i < 3; i++) {
+        for(int j =0; j< 3; j++){
+        somamatriz[i][j] = matriz[i][j] + matriz[i][j];
+        
+        printf(" %d", somamatriz[i][j]);
+    }
+    
+    printf(" \n");
+    printf(" ");
+    }
+   int matriz[3][3] = {{9,15,21}, {2,4,6},{2,3,5}};
+   for(int i = 0; i < 3; i++) {
+        for(int j =0; j< 3; j++){
+        
+        printf(" %d ", matriz[i][j]);
+        printf(" ");
+        }
+        printf("\n");
+        printf(" ");
+    }*/
+   #define LINHAS 10 // define constantes e basta mudar aqui para alterar por todo o código
+   #define COLUNAS 10
+   int matriz[LINHAS][COLUNAS];
+   int soma = 0;
+
+   for(int i = 0; i < LINHAS; i++){
+    for(int j = 0; j < COLUNAS; j++){
+        soma = i+j;
+        matriz[i][j] = soma;
+        printf(" %-4d ", matriz[i][j]);// para controlar o tamanho das colunas
+    }
+    printf("\n");
+   }
+   printf("\n");
     return 0;
 }
