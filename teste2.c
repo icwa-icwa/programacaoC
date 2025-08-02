42d044d1b41dc1168d256984017f42d9bd856ad3
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-08-02 00:11:33 +0000

    incrementos

--- teste2.c
+++ teste2.c
@@ -7,6 +7,7 @@ int main(){
     Atribuição com Multiplicação (*=)
     Atribuição com Divisão(/=)
     */
+   /*
    int numero1 = 10, numero2, resultado;
    resultado = 10;
    printf("Resultado: %d\n", resultado);
@@ -18,4 +19,29 @@ int main(){
    printf("Resultado: %d\n", resultado);
    resultado /= 2; // resultado = resultado / 2;
    printf("Resultado: %d\n", resultado);
+   */
+  /*
+  Incremento(++)
+  Pré-Incremento ++a
+  Pós-Incremento a++
+  Decremento(--)
+  Pré-Decremento --a
+  Pós-Decremento a--
+  */
+ int numero1 = 1, resultado;
+ printf("Antes incremento: %d\n", numero1); // 3 formas de usar incremento ou decremento
+ // numero1 = numero1 +1;
+ // numero1 += 1;
+ resultado = numero1++;
+ numero1++;
+ //printf("Pós incremento: %d\n", numero1);
+ 
+ numero1--;
+ printf("Pós decremento: %d\n", numero1);
+ // numero1 = numero1 -1;
+ // numero1 -= 1;
+ ++numero1;
+printf("Pré incremento: %d\n", numero1);
+--numero1;
+printf("Pré decremento: %d\n", numero1);
 }
