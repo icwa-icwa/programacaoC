960145a74f03b59f34dfd1a5deaf80d85dae7857
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-08-04 13:15:47 +0000

    aninhada

---
+++ .vscode/logico.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+int main(){
+   /*int a = 10, b = 5;
+    if (a > 0 || b > 0) 
+    { printf("Pelo menos um dos números é positivo]n");
+    } else {
+        printf("Os dois números são negativos.\n");
+    }
+// precedencia de operadores
+// estrutura de decisao encadeadas
+int a = - 5, b = - 6;
+if(a > 0 )
+{ 
+    printf("a e positivo.\n");
+    }
+    
+if(b > 0 ){
+    printf("b e positivo.\n");
+} else {
+    printf("pelo menos um numero e negativo.\n");
+}
+// estrutura de decisao aninhada
+int idade, renda;
+printf("Digite sua idade:");
+scanf("%d", &idade);
+printf("Digite a sua renda mensal:");
+scanf("%d", &renda);
+
+if (idade <= 18 || idade >= 60){
+    if(renda < 2000){
+    printf("Você tem direito ao desconto.\n");
+} else {
+    printf("Você não atende aos critérios pela renda.\n");
+} 
+} else {
+    printf("Você não atende aos critérios pela idade.\n");
+}
+int idade, renda, dependentes;
+printf("Digite sua idade:\n");
+scanf("%d", &idade);
+printf("Digite sua renda:\n");
+scanf("%d", &renda);
+printf("Digite o número de dependentes:\n");
+scanf("%d", &dependentes);
+
+if(idade >= 18 && idade <= 65){
+    if(renda < 3000){
+        if(dependentes > 2){
+            printf("Você atende todos os critérios.\n");
+        }
+    } else{
+        printf("Você não atende ao critério renda.\n");
+}
+}else{
+    printf("Você não atende ao critério idade.\n");
+}
+int numero;
+printf("Digite um número:\n");
+scanf("%d", &numero);
+
+if (numero > 0){
+    if(numero % 2 == 0){
+        printf("o número é par\n");
+    } else {
+        printf("o número é ímpar\n");
+    }
+} else if(numero == 0){
+    printf("o número é zero\n");
+} else {
+    printf("negativo\n");
+}*/
+
+
+    return 0;
+
+}
