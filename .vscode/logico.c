a6675a1168e6a9c8e121c8e6e00c25997d9c0d49
ILSE CONCEICAO DE WITT DE AZEVEDO <icwa.icwa@gmail.com>
2025-08-05 14:14:53 +0000

    integracao2

--- .vscode/logico.c
+++ .vscode/logico.c
@@ -112,8 +112,15 @@ float saldo = 1000;
     printf("Sábado\n");
    }*/
    // pode ser feito com o switch
-float nota1, nota2;
-int opcao;
+float nota1, nota2, media;
+int opcao;  
+    printf("Menu de Gerenciamento de Estudantes\n");
+    printf("1.Calcular a média\n");
+    printf("2.Determinar Status\n");
+    printf("3.Sair\n");
+    printf("Escolha uma opção: \n");
+    scanf("%d", &opcao);
+   
 switch(opcao)
 {
     case 1:
@@ -122,15 +129,26 @@ switch(opcao)
     scanf("%f",&nota1);
     printf("Digite a segunda nota:\n");
     scanf("%f", &nota2);
-    // testar a conidição se a nta é >= 0 e <= 10
-    if ((nota1>=0 && nota1<=10) && (nota2>=0 && nota2<=10)){
-        printf("Entrada correta.\n");
-    }else{
-        printf("Entrada com valores errados.\n");
+    // testar a condição se a nota é >= 0 e <= 10
+    if((nota1>=0 && nota1 <=10) && (nota2>=0 && nota2 <=10) )
+    {
+        media = (nota1 + nota2)/2;
+        printf("A média é: %.2f\n", media);
+    }else {
+        printf("Entradas com Notas Incorretas\n");
     }
     break;
     case 2:
     printf("Determinar status\n");
+    printf("Entrar com a média: \n");
+    scanf("%f", &media);
+    if(media >= 7 && media <= 10){
+        printf("Status: Aprovado\n");
+    } else if(media >=5){
+        printf("Status: Recuperação\n");
+    } else{
+        printf("Status: Reprovado\n");
+    }
     break;
     case 3:
     printf("Saindo do programa...\n");
@@ -139,7 +157,7 @@ switch(opcao)
     printf("Opção inválida\n");
     break;
 }
-
+    printf("\n");
 
     return 0;
 
